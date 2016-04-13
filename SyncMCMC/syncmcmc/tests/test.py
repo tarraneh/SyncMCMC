8589fd5899261abe437db630f538a629348d36d0
tarraneh <teftekhari@cfa.harvard.edu>
2016-04-13 10:32:01 -0400

    Delete test.py

--- SyncMCMC/syncmcmc/tests/test.py
+++
@@ -1,9 +0,0 @@
-
-from unittest import TestCase
-
-from syncmcmc import testprog as tp
-
-class TestMe(TestCase):
-    def i_am_string(self):
-        s = tp.myfunc()
-        self.assertTrue(isinstance(s,basestring))
