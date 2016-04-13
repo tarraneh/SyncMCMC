6ffbcd5c43faaf859678cf6a525f66e83e361fea
tarraneh <teftekhari@cfa.harvard.edu>
2016-04-12 20:23:01 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='syncmcmc',
+      version='0.1',
+      description='Synchrotron Modeling using MCMC',
+      url='http://github.com/phys201-sp2016/SyncMCMC',
+      author='Tarraneh Eftekhari',
+      author_email='teftekhari@cfa.harvard.edu',
+      packages=['syncmcmc'],
+      install_requires=[
+          'numpy',
+          'matplotlib',
+          'emcee',
+      ],
+      zip_safe=False)
