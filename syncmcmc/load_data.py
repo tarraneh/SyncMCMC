50ee17b473ca441e61c74171d1fedfd9fd3f329a
tarraneh <teftekhari@cfa.harvard.edu>
2016-04-17 12:35:05 -0400

    Update load_data.py

--- syncmcmc/load_data.py
+++ syncmcmc/load_data.py
@@ -11,25 +11,3 @@ class Fluxes:
 		self.frequency = frequency
 		self.flux = flux
 		self.error = error
-
-
-
-
-	def readClassFile(filename,Fluxes, separator="\t", comment='#'):
-		try:
-			file = open(filename, "r")
-		except:
-			raise IOError, "Can't open file %s for reading" % filename
-
-		for line in file:
-			if comment is not None and line.startswith(comment):
-				continue
-
-		tuple = line.rstrip().split(separator)
-		try:
-			Fluxes(tuple)
-			return Fluxes(tuple)
-		except ValueError:
-			raise IOError, "Line of file %s does not have the required format: \n%s" % (filename, line)
-
-
