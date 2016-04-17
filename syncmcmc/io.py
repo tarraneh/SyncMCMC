b94588a6083791664a93206352e050faa78195c2
tarraneh <teftekhari@cfa.harvard.edu>
2016-04-17 12:36:22 -0400

    Create io.py

---
+++ syncmcmc/io.py
@@ -0,0 +1,20 @@
+from load_data import Fluxes
+
+def readClassFile(filename,Fluxes, separator="\t", comment='#'):
+	try:
+		file = open(filename, "r")
+	except:
+		raise IOError, "Can't open file %s for reading" % filename
+	for line in file:
+		if comment is not None and line.startswith(comment):
+			continue
+	tuple = line.rstrip().split(separator)
+	try:
+		Fluxes(tuple)
+		return Fluxes(tuple)
+	except ValueError:
+		raise IOError, "Line of file %s does not have the required format: \n%s" % (filename, line)
+
+
+s = readClassFile("10days",Fluxes)
+print s.error
