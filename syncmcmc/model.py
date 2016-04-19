19b77437d685dd416c30ca6acb20841a85091dd8
tarraneh <teftekhari@cfa.harvard.edu>
2016-04-18 22:57:47 -0400

    Update model.py

--- syncmcmc/model.py
+++ syncmcmc/model.py
@@ -12,7 +12,7 @@ F_true = 10**(0.96)
 va_true = 10**(10.11)
 vm_true = 10**(11.41)
 
-priors = FluxFrequencyPrior(UniformPrior(1,55),UniformPrior(1E9,1E13),UniformPrior(1E9,1E13))
+priors = FluxFrequencyPriors(UniformPrior(1,55),UniformPrior(1E9,1E13),UniformPrior(1E9,1E13))
 
 
 
