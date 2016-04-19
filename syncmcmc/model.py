643988778853ca9caf3273993ca622a0e3bd36eb
tarraneh <teftekhari@cfa.harvard.edu>
2016-04-18 23:15:19 -0400

    Update model.py

--- syncmcmc/model.py
+++ syncmcmc/model.py
@@ -146,5 +146,5 @@ pos = np.column_stack((frand,varand,vmrand,yerrand))
 pos_add_dim = np.expand_dims(pos,axis=0)
 final_pos = np.repeat(pos_add_dim, 5, axis=0)
 
-sampler = emcee.PTSampler(5, nwalkers, ndim, lnlike, priors.lnprior, loglargs=[freqs,flux,error],logpargs=[F_v,v_a,v_m])
+sampler = emcee.PTSampler(5, nwalkers, ndim, lnlike, priors.lnprior, loglargs=[freqs,flux,error],logpargs=[*args])
 sams = sampler.run_mcmc(final_pos, 1000)
