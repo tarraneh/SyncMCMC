04763442ec8c9deaed182e51bbcce18ff651c0a1
tarraneh <teftekhari@cfa.harvard.edu>
2016-04-18 20:28:16 -0400

    Update test.py

--- syncmcmc/tests/test.py
+++ syncmcmc/tests/test.py
@@ -4,10 +4,6 @@ import unittest
 import syncmcmc
 from priorclasses import UniformPrior
 
-#class TestFillerFunc(TestCase):
- #   def test_is_string(self):
-  #      s = syncmcmc.fillerfunc()
-   #     self.assertTrue(isinstance(s, str))
 
 class UniformPriorTestCase(unittest.TestCase):
    def test_UniformPriors(self):
@@ -15,7 +11,7 @@ class UniformPriorTestCase(unittest.TestCase):
         # Define a uniform prior between 1 and 10
         test = UniformPrior(1,10)
         # Is 5 contained within the prior bounds?
-        self.assertFalse(test.lnprior(500))
+        self.assertFalse(test.lnprior(5))
 
 
 if __name__ == '__main__':
