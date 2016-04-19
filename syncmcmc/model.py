2711b1906fd94f796388fe11b634da940117bd39
tarraneh <teftekhari@cfa.harvard.edu>
2016-04-18 22:53:46 -0400

    Update model.py

--- syncmcmc/model.py
+++ syncmcmc/model.py
@@ -8,7 +8,9 @@ import os
 import argparse
 from PTmcmc import run_mcmc
 
-
+F_true = 10**(0.96)
+va_true = 10**(10.11)
+vm_true = 10**(11.41)
 
 
 
@@ -126,4 +128,21 @@ def lnprob(theta, v, y, yerr, prior):
         return -np.inf
     return lp + lnlike(theta, v, y, yerr)
     
-run_mcmc()
+# Define number of dimensions and number of walkers
+
+ndim, nwalkers = 4, 100
+
+
+# Define initial positions of walkers in phase space
+
+ frand = np.random.normal(loc=F_true,size=nwalkers,scale=0.1)
+ varand = np.random.normal(loc=va_true,size=nwalkers,scale=1.E3)
+ vmrand = np.random.normal(loc=vm_true,size=nwalkers,scale=1.E3)
+ yerrand = np.random.normal(loc=-0.7,size=nwalkers,scale=0.1)
+ 
+ pos = np.column_stack((frand,varand,vmrand,yerrand)) 
+ pos_add_dim = np.expand_dims(pos,axis=0)
+ final_pos = np.repeat(pos_add_dim, 5, axis=0)
+ 
+ sampler = emcee.PTSampler(5, nwalkers, ndim, lnlike, p.lnprior, loglargs=[v3,flux3,err3])
+ sams = sampler.run_mcmc(final_pos, 1000)
