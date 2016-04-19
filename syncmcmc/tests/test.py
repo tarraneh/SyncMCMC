eaa570934c71ecc0d27982ec66fb1606ab2fbc92
tarraneh <teftekhari@cfa.harvard.edu>
2016-04-18 20:08:19 -0400

    Update test.py

--- syncmcmc/tests/test.py
+++ syncmcmc/tests/test.py
@@ -2,7 +2,7 @@
 from unittest import TestCase
 import unittest
 import syncmcmc
-from data_class.py import UniformPrior
+from data_class import UniformPrior
 
 class TestFillerFunc(TestCase):
     def test_is_string(self):
