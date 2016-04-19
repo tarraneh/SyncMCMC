f4312cdbabd7a7f5d077f939569c98ce944514d8
tarraneh <teftekhari@cfa.harvard.edu>
2016-04-18 20:31:36 -0400

    Update test.py

--- syncmcmc/tests/test.py
+++ syncmcmc/tests/test.py
@@ -9,9 +9,16 @@ class UniformPriorTestCase(unittest.TestCase):
    def test_UniformPriors(self):
         """Tests UniformPrior class"""
         # Define a uniform prior between 1 and 10
-        test = UniformPrior(1,10)
+        test_uniform_priors = UniformPrior(1,10)
         # Is 5 contained within the prior bounds?
-        self.assertFalse(test.lnprior(5))
+        self.assertFalse(test_uniform_priors.lnprior(5))
+     
+        
+class FluxFrequencyPriorsTestCase(unittest.TestCase):
+   def test_FluxFrequencyPriors(self):
+      '''Tests FluxFrequencyPriors class'''
+      test_all_priors = FluxFrequencyPriors(UniformPrior(1.,50.),UniformPrior(1.E10,1.E13),UniformPrior(1.E10,1.E13))
+      self.assertFalse(test_all_priors.lnprior(10.,1.E11.,1.E12.))
 
 
 if __name__ == '__main__':
